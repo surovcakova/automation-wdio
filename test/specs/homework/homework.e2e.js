117b1b926086483e9faa8bdb727415259a4ca325
Marie Surovcakova <marie.surovcakova@seznam.cz>
2023-07-18 14:26:30 +0200

    homework_01

--- test/specs/homework/homework.e2e.js
+++ test/specs/homework/homework.e2e.js
@@ -2,7 +2,12 @@ describe('Homework', async () => {
 
     it('should open page and create screenshot', async () => {
 
+    await browser.reloadSession();
+
     await browser.url ("/registrace")
+
+    const windowSize = await browser.getWindowSize();
+    console.log(windowSize);
 
     await browser.saveScreenshot('register_page.png');
 
